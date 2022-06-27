479b9495eda60c7423d8f893064fd110495e23db
Holly McGuinness <gaithelema@gmail.com>
2022-06-27 01:49:49 -0400

    added regex to password file

--- password.js
+++ password.js
@@ -9,10 +9,33 @@
 let password = (`itsBritn3y`)
 
 //  if length is >= 10 && contains at least one letter && one number
+// if (conditions) {
+    // execution block
+//} else {
+    // execution block
+//}
 
-If ((password.length >= 10) && (password.search(/[0-9]/) < 0) && (password.search(/[a-zA-Z]/) < 0 )) {
-    console.log(`Success, you're incredible.`)
-} else {
-    console.log (`Failure. What a disappointment.`)
-}
+// Variables
 
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+reader.question(`Enter password for validation.`, function(input){
+
+    const regex = /\d/;
+    const hasNumber = regex.test(input)
+
+    const regex2 = /[a-zA-Z]/
+    const hasLetter = regex2.test(input)
+
+    if (input.length >= 10 && hasNumber === true && hasLetter === true) {
+        console.log(`This password is elite.`)
+    } else {
+        console.log(`Sad trombone; no access for you.`)
+    }
+
+})
